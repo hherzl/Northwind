af863312be8efeb640ab19daa3f5b2d3ad270aca
cportillosv <cportillosv@outlook.com>
2014-12-04 00:03:49 -0600

    add AngularJS  NortwindWebApi2

---
+++ NorthwindWebApi2/Angular/app.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', []);
+
+    app.controller('ProductController', [
+     '$http', '$scope', function ($http, $scope) {
+
+         $scope.products = [];
+         $http.get('http://localhost:58532/api/Product').success(function (data) {
+             $scope.products = data;
+         });
+
+     }
+    ]);
+
+   })();
