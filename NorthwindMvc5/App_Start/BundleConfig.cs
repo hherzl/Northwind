c211f05aecce50392c4692e788bc0132f3251aff
hherzl <hans.herzl@live.com>
2015-06-04 23:31:55 -0600

    Inlcude of bootstrap-theme file in bundle config

--- NorthwindMvc5/App_Start/BundleConfig.cs
+++ NorthwindMvc5/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace NorthwindMvc5
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-theme.css",
                       "~/Content/site.css"));
         }
     }
--- NorthwindWebApi2/App_Start/BundleConfig.cs
+++ NorthwindWebApi2/App_Start/BundleConfig.cs
@@ -22,6 +22,7 @@ namespace NorthwindWebApi2
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-theme.css",
                       "~/Content/site.css"));
         }
     }
