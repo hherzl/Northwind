6a27218cf2c339145d643cfa0f03103e86eda38e
hherzl <hans.herzl@live.com>
2015-09-14 23:24:27 -0600

    Feature: addition of unit test to Web API controller

---
+++ NorthwindWebApi2.Tests/Controllers/ShipperControllerTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NorthwindWebApi2.Controllers;
+using NorthwindWebApi2.Models;
+using NorthwindWebApi2.Services;
+
+namespace NorthwindWebApi2.Tests.Controllers
+{
+    [TestClass]
+    public class ShipperControllerTest
+    {
+        [TestMethod]
+        public async Task Get()
+        {
+            // Arrange
+            var controller = new ShipperController(new UowService());
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            var httpResponse = await controller.Get();
+
+            var shipperResponse = default(ApiResponse);
+
+            httpResponse.TryGetContentValue<ApiResponse>(out shipperResponse);
+
+            // Assert
+            Assert.IsNotNull(shipperResponse);
+        }
+
+        [TestMethod]
+        public async Task GetById()
+        {
+            // Arrange
+            var controller = new ShipperController(new UowService());
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            var httpResponse = await controller.Get(1);
+
+            var shipperResponse = default(ApiResponse);
+
+            httpResponse.TryGetContentValue<ApiResponse>(out shipperResponse);
+
+            // Assert
+            Assert.IsNotNull(shipperResponse);
+        }
+    }
+}
