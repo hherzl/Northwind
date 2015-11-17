9c35402334d65fb0909b6ab107d1ac25bc8dc08c
hans.hernandez <hans.hernandez@m-banco.com>
2015-11-17 16:38:39 -0600

    Code improvement: addition of wcf service

---
+++ NorthwindService/INorthwindService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using Northwind.Core.EntityLayer;
+
+namespace NorthwindService
+{
+    [ServiceContract]
+    public interface INorthwindService
+    {
+        [OperationContract]
+        IEnumerable<Customer> GetCustomers();
+    }
+}
---
+++ NorthwindService/NorthwindServiceImplementation.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Northwind.Core.DataLayer;
+using Northwind.Core.DataLayer.Contracts;
+using Northwind.Core.EntityLayer;
+
+namespace NorthwindService
+{
+    public class NorthwindServiceImplementation : INorthwindService
+    {
+        protected ISalesUow Uow;
+
+        public NorthwindServiceImplementation()
+        {
+            Uow = new SalesUow(new SalesDbContext());
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return Uow.CustomerRepository.GetAll().ToList();
+        }
+    }
+}
