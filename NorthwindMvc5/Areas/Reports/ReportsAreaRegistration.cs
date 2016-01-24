67115e7010159ac65786128ea7d2a17c2581c4e3
hherzl <hans.herzl@live.com>
2016-01-24 13:43:58 -0600

    Code feature: addition of reports area

---
+++ NorthwindMvc5/Areas/Reports/ReportsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace NorthwindMvc5.Areas.Reports
+{
+    public class ReportsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Reports";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Reports_default",
+                "Reports/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
